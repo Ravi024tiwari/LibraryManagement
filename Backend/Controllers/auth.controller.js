275b040ef39b950387b4e7e0c6021e62b134454e
Ravi Tiwari <raviashoktiwari9559@gmail.com>
2026-01-18 21:25:12 +0530

    fix:cookie

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -79,6 +79,7 @@ const login =async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             sameSite: "none",
+            secure: true,
             maxAge: 1 * 24 * 60 * 60 * 1000
         })
 
